97a74710d58a1d9223af9ab97f4d468b8d8228d6
Alex M <arm@Alexs-MBP.lan>
2022-06-01 17:07:40 +0200

    Renamed some variables to indicate the generic nature of the project

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import git
 from datetime import datetime
 
 
-WEBFLOW_SOURCE_JSON_DIR = "/tmp/airbyte_local/webflow-collections"
-WEBFLOW_DEST_JSON_DIR = "/Users/arm/Documents/test-webflow-backup-1"
+AIRBYTE_LOCAL_SOURCE_JSON = "/tmp/airbyte_local/webflow-collections"
+DEST_DIR_FOR_GITHUB = "/Users/arm/Documents/test-webflow-backup-1"
 
 """ 
 WARNING: Before executing this code, ensure that git has been initialized in the destination directory 
 as instructed below. 
 
 Create a new repository on Github, and manually create a new git repository
-inside the directory: <WEBFLOW_DEST_JSON_DIR>. Just follow instructions from github such as the following:
+inside the directory: <DEST_DIR_FOR_GITHUB>. Just follow instructions from github such as the following:
 
 echo "# <your github repo name>" >> README.md
 git init
@@ -38,11 +38,11 @@ def loop_over_jsonl_and_write_to_output(source_filename_with_path, destination_f
 
 def walk_json_files():
 
-    for root, dirs, files in os.walk(WEBFLOW_SOURCE_JSON_DIR):
+    for root, dirs, files in os.walk(AIRBYTE_LOCAL_SOURCE_JSON):
         for name in files:
             source_filename_with_path = os.path.join(root, name)
             destination_folder_name= os.path.splitext(name)[0]
-            destination_folder_name_with_path = f"{WEBFLOW_DEST_JSON_DIR}/{destination_folder_name}"
+            destination_folder_name_with_path = f"{DEST_DIR_FOR_GITHUB}/{destination_folder_name}"
             if not os.path.exists(destination_folder_name_with_path):
                 os.makedirs(destination_folder_name_with_path)
 
@@ -53,12 +53,12 @@ def push_to_github():
 
     try:
         # open the git repo if it exists
-        repo = git.Repo(WEBFLOW_DEST_JSON_DIR)
+        repo = git.Repo(DEST_DIR_FOR_GITHUB)
     except git.InvalidGitRepositoryError:
         # create the repo if doesn't exist
         print("Please create the git repository on github and then locally")
 
-    repo.git.add(f"{WEBFLOW_DEST_JSON_DIR}/.")
+    repo.git.add(f"{DEST_DIR_FOR_GITHUB}/.")
     try:
         repo.git.commit(m=f"Commit by script: {__file__} on {datetime.now().isoformat()}")
     except Exception as e:
